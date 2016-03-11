b9d4b66065e793354b42aa9d7f24f5ed15875de4
VilleViktor <viktor.k.larsson@outlook.com>
2016-03-11 22:10:56 +0100

    Create languages.js

---
+++ language/languages.js
@@ -0,0 +1,10 @@
+/**
+ * Created by viktorlarsson on 2016-03-11.
+ * Gather all the languages
+ */
+
+var language = {};
+language.swedish = require('./swedish').swedish;
+language.english = require('./english').english;
+
+exports.language = language;
