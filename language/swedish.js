6ec9ed8fefa808b51e257997df7b42a6ca9e9759
VilleViktor <viktor.k.larsson@outlook.com>
2016-03-12 00:04:52 +0100

    Update swedish.js

--- language/swedish.js
+++ language/swedish.js
@@ -4,8 +4,8 @@
  
 var badSwedish =
     [  
-       "blatte", "bög","fan", "fitta","helvete","jävel", "jävla", "knulla", "kuk","kåt","sugen", "lebb", "neger", "röv", 
-       "snopp","sug min", "penis","vagina"
+       "anal","anus","balle","blatte","bög","fan", "fitt","fitta","flata","helvete","hora","jävel", "jävla", "knulla", "kuk","kåt",
+       "sugen","lebb","nazi", "neger","ollon","pung","runk", "röv",  "snopp","sug min", "penis","vagina"
     ];
 
 exports.swedish = badSwedish;
