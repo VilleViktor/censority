e1084c248475afc7f9f20a2b70313e09d4c24cc9
VilleViktor <viktor.k.larsson@outlook.com>
2016-03-11 22:10:27 +0100

    Create swedish.js

---
+++ language/swedish.js
@@ -0,0 +1,10 @@
+/**
+ * Created by viktorlarsson on 2016-03-11.
+ */
+ 
+var badSwedish =
+    ["knulla", "kuk", "fitta", "jävla", "fan", "vagina",
+    "helvete", "neger", "bög", "lebb", "snopp", "penis",
+        "blatte", "jävel"];
+
+exports.swedish = badSwedish;
